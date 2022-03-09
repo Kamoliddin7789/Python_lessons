43a468c870563d05803b92d1d1b446d516aa3301
Kamoliddin <98181793+Kamoliddin7789@users.noreply.github.com>
2022-03-09 23:01:47 +0500

    Delete dars_7.py

--- dars_7.py
+++
@@ -1,4 +0,0 @@
-mevalar=["anor","o'rik","shaptoli","anjir"]#str dan iborat elementlar
-narxlar=[12000,4000,5000,64000,88800]#int dan iborat elemantlar
-isimlar=["anvar", "obidov", 1300, 1400]#str va int dan iborat elemntlar
-puste=[]#bo'sh ro'yhat
